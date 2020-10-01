035b2b3601c787192f9eb65c2e4732117a358cc6
flybirdgroup <flybirdgroup@163.com>
2020-10-01 09:10:22 +0800

    updated dataflow- mvp

--- bq.py
+++ bq.py
@@ -15,7 +15,7 @@ logger = logging.getLogger(__name__)
 table_spec = bigquery.TableReference(
     projectId='query-11',
     datasetId='rpm',
-    tableId='account_id_schema_new')
+    tableId='ahaha')
 
 dataflow_options = ['--project=query-11','--job_name=amaz','--temp_location=gs://dataflow_s/tmp','--region=us-central1']
 dataflow_options.append('--staging_location=gs://dataflow_s/stage')
@@ -25,25 +25,27 @@ gcloud_options = options.view_as(GoogleCloudOptions)
 options.view_as(StandardOptions).runner = "dataflow"
 
 table_schema = {
-    'fields': [
-        {'name': 'ACNO', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_1', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_2', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_3', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_4', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_5', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_6', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_7', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_8', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_9', 'type': 'STRING', 'mode': 'NULLABLE'},
-        {'name': 'FIELD_10', 'type': 'STRING', 'mode': 'NULLABLE'},
-    ]
+    'fields': [{
+        'name': 'source', 'type': 'STRING', 'mode': 'NULLABLE'
+    }, {
+        'name': 'quote', 'type': 'STRING', 'mode': 'REQUIRED'
+    }]
 }
 
+
+
 with beam.Pipeline(options=options) as p:
-    max_temperatures = (
-        p
-        | 'ReadTable' >> beam.io.Read(beam.io.BigQuerySource(table_spec)))
+    quotes = p | beam.Create([
+        {
+            'source': 'Mahatma Gandhi', 'quote': 'My life is my message.'
+        },
+        {
+            'source': 'Yoda', 'quote': "Do, or do not. There is no 'try'."
+        },
+    ])
 
-max_temperatures | 'WriteTable' >> beam.io.WriteToBigQuery(table_spec, schema=table_schema,write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
-
+    quotes | beam.io.WriteToBigQuery(
+        table_spec,
+        schema=table_schema,
+        write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
+        create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
